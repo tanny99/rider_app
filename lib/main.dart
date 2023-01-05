4ad8f9fb085859e15318c4aa61914939360d2088
Tanishk Gupta <tanishkgupta10@gmail.com>
2023-01-05 15:09:11 +0530

    UI of Rider App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Rider App',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: const MyHomePage(title: 'Rider App'),
-    );
-  }
+  _State createState() => _State();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _State extends State<MyApp> {
+  final List<String> names = <String>['Robert', 'Micheal'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-
-          ],
+        appBar: AppBar(
+          title: Text('Rider App'),
+          backgroundColor: Colors.brown,
         ),
-      ),
+        body: Column(
+          children: [
+          Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey)
+                    ),
+                    height: 50,
+                    margin: EdgeInsets.all(2),
 
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text('${names[index]}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+
+                    ),
+
+                  );
+                }
+            ),
+          ),
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(38.0),
+                child: SizedBox(
+                  width: 100.0,
+                  height: 50.0,
+
+                  child: ElevatedButton(onPressed: (){}, child: Text('Add'),style: ButtonStyle(
+    backgroundColor: MaterialStateProperty.all(Color(0xFF6558F5)),)),
+                ),
+              ),
+            )
+          ],
+        )
     );
   }
 }
